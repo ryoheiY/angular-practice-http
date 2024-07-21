be49ed17dd76c1a9ea4f97479532a3d0831d0d0f
ryoheiY <momoyamashia@icloud.com>
2024-07-21 23:15:43 +0900

    HTTPリクエストにinterceptorを追加

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import {provideHttpClient} from "@angular/common/http";
+import {HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors} from "@angular/common/http";
+
+function loggingInterceptor(request: HttpRequest<any>, next: HttpHandlerFn) {
+  console.log("Request interceptor");
+  console.log(request);
+  return next(request);
+}
 
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([
+      loggingInterceptor,
+    ])),
   ],
 }).catch((err) => console.error(err));
