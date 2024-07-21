c224dd56cce16a23ce9a7a7297f57135d505f54a
ryoheiY <momoyamashia@icloud.com>
2024-07-21 20:35:45 +0900

    Http ini

---
+++ src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,40 @@
+import {Component, DestroyRef, inject, OnInit, signal} from '@angular/core';
+
+import {Place} from '../place.model';
+import {PlacesComponent} from '../places.component';
+import {PlacesContainerComponent} from '../places-container/places-container.component';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+
+})
+export class AvailablePlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>("http://localhost:3000/places", {
+        observe: 'response'
+      })
+      .subscribe({
+        next: resultData => {
+          console.log(resultData);
+          console.log(resultData.body?.places);
+
+        }
+      })
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
+
+
+}
